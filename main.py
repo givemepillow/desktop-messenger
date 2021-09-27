60b94ae7edc1d01a731fe3854de742f315705954
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-27 21:07:38 +0300

    controller as init qml file.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import sys
+# This Python file uses the following encoding: utf-8
 import os
+from pathlib import Path
+import sys
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
+
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load(os.path.join(os.path.dirname(__file__), "qml\init.qml"))
-
+    engine.load(os.fspath(
+            Path(__file__).resolve().parent / "qml/controller.qml")
+        )
     if not engine.rootObjects():
         sys.exit(-1)
     sys.exit(app.exec_())
