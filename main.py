28fb2d70a1146945e3737ce4ec8973ecedb7f9c0
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-30 22:07:05 +0300

    Added simple authentication.

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import sys
 
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
+#from PySide2.QtCore import QObject, QUrl, Slot
 
+from authentication import Verify
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
+    v = Verify()
     engine = QQmlApplicationEngine()
-    engine.load(os.fspath(
-            Path(__file__).resolve().parent / "qml/controller.qml")
-        )
+    engine.rootContext().setContextProperty("v", v)
+    engine.load(os.fspath(Path(__file__).resolve().parent / "qml/controller.qml"))
     if not engine.rootObjects():
         sys.exit(-1)
     sys.exit(app.exec_())
