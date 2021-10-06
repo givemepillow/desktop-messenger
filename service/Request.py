ef962248d7543342fb5dc2980db7cd6d081071dd
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-07 02:56:19 +0300

    Added registration request.

--- service/Request.py
+++ service/Request.py
@@ -2,6 +2,7 @@ import json
 
 class Request:
     AUTHENTICATION_REQUEST = 1
+    REGISTARTION_REQUEST = 2
 
     @classmethod
     def authentication(cls, login, password):
@@ -9,4 +10,15 @@ class Request:
             'request_type': cls.AUTHENTICATION_REQUEST,
             'login': login,
             'password': password
+        })
+
+    @classmethod
+    def registration(cls, login, password, first_name, last_name, email):
+        return json.dumps({
+            'request_type': cls.REGISTARTION_REQUEST,
+            'login': login,
+            'password': password,
+            'email': email,
+            'first_name': first_name,
+            'last_name': last_name
         })
