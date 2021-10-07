da94d3ab84ffea17196dff00451b4e9eaf9d7cc4
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-07 19:04:09 +0300

    Request types class.

--- service/Request.py
+++ service/Request.py
@@ -2,23 +2,4 @@ import json
 
 class Request:
     AUTHENTICATION_REQUEST = 1
-    REGISTARTION_REQUEST = 2
-
-    @classmethod
-    def authentication(cls, login, password):
-        return json.dumps({
-            'request_type': cls.AUTHENTICATION_REQUEST,
-            'login': login,
-            'password': password
-        })
-
-    @classmethod
-    def registration(cls, login, password, first_name, last_name, email):
-        return json.dumps({
-            'request_type': cls.REGISTARTION_REQUEST,
-            'login': login,
-            'password': password,
-            'email': email,
-            'first_name': first_name,
-            'last_name': last_name
-        })
+    REGISTRATION_REQUEST = 2
