9419da6892075be50f1d2710427d3c711873a65c
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-08 03:14:04 +0300

    Added server message support.

--- service/Dispatcher.py
+++ service/Dispatcher.py
@@ -5,34 +5,47 @@ from .AnswerParser import AnswerParser
 from .Answer import Answer
 
 class Account(QObject):
+
+    server_message = ''
+
     @Slot(str, str, result=bool)
     def authentication(self, login, password):
         Service.connect()
         Service.send(RequestConstructor.authentication(login, password))
-        answer = Service.receive()
+        answer = AnswerParser.extract_answer(Service.receive())
         Service.close()
-        return AnswerParser.assert_answer(answer, Answer.ACCEPT)
+        self.server_message = answer['message']
+        return answer['type'] == Answer.ACCEPT
 
     @Slot(str, str, str, str, str, result=bool)
     def registration(self, login, password, first_name, last_name, email):
         Service.connect()
         Service.send(RequestConstructor.registration(login, password, first_name, last_name, email))
-        answer = Service.receive()
+        answer = AnswerParser.extract_answer(Service.receive())
         Service.close()
-        return AnswerParser.assert_answer(answer, Answer.ACCEPT)
+        self.server_message = answer['message']
+        return answer['type'] == Answer.ACCEPT
 
     @Slot(str, str, result=bool)
     def verification(self, email, code):
         Service.connect()
         Service.send(RequestConstructor.verification(email, code))
-        answer = Service.receive()
+        answer = AnswerParser.extract_answer(Service.receive())
         Service.close()
-        return AnswerParser.assert_answer(answer, Answer.ACCEPT)
+        self.server_message = answer['message']
+        return answer['type'] == Answer.ACCEPT
 
     @Slot(str, str, result=bool)
     def email_and_login(self, email, login):
         Service.connect()
         Service.send(RequestConstructor.email_and_login(email, login))
-        answer = Service.receive()
+        answer = AnswerParser.extract_answer(Service.receive())
         Service.close()
-        return AnswerParser.assert_answer(answer, Answer.ACCEPT)
+        self.server_message = answer['message']
+        return answer['type'] == Answer.ACCEPT
+
+    @Slot(result=str)
+    def get_server_message(self):
+        msg = self.server_message
+        self.server_message = ""
+        return msg
