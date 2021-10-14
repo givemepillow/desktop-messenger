2d8284c9724e92c9c284ba3be2d7d5ad60cd2425
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-14 21:20:35 +0300

    Service -> RemoteServer. New interface.

--- service/RemoteServer.py
+++ service/RemoteServer.py
@@ -1,16 +1,20 @@
 import socket
 
-class Service:
+__all__ = ['RemoteServer']
 
-    BUFFER_SIZE = 1024
+
+class Network:
+    BUFFER_SIZE = 1024 * 10
     PORT = 6767
     ADDRESS = '127.0.0.1'
+
+    __socket = None
 
     @classmethod
     def connect(cls):
         try:
             cls.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            cls.__socket.connect((cls.ADDRESS, cls.PORT),)
+            cls.__socket.connect((cls.ADDRESS, cls.PORT), )
         except ConnectionRefusedError as e:
             print(e)
 
@@ -22,7 +26,18 @@ class Service:
     def send(cls, data):
         cls.__socket.sendall(bytes(data, encoding="utf-8"))
 
-    @classmethod 
+    @classmethod
     def receive(cls):
         data = cls.__socket.recv(cls.BUFFER_SIZE)
         return data.decode("utf-8")
+
+
+class RemoteServer:
+    @classmethod
+    def data_exchange(cls, data):
+        Network.connect()
+        Network.send(data)
+        rec_data = Network.receive()
+        Network.close()
+        return rec_data
+
