373d87bcdaed011b1f434a308dfc94717e5052e0
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-23 17:23:39 +0300

    JS WindowManager - createWindow fucntion.

---
+++ qml/windowManager.js
@@ -0,0 +1,16 @@
+var CURRENT_WINDOW = null;
+
+function replaceWindows(newWindow) {
+    if (CURRENT_WINDOW !== null) {
+        console.log("close")
+        CURRENT_WINDOW.close();
+    }
+    CURRENT_WINDOW = newWindow;
+    CURRENT_WINDOW.show();
+}
+
+function createWindow(componentFile) {
+    let component = Qt.createComponent(componentFile);
+    let window = component.createObject();
+    replaceWindows(window);
+}
