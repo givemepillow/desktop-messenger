07b8ee285357147fd388bbb4d59e79d7d4f4ec7e
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-29 17:32:53 +0300

    Service to service qml.

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtCore import QUrl
 
+from service import Service
 
 if __name__ == '__main__':
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    
+
+    s = Service()
+    engine.rootContext().setContextProperty("service", s)
+
     engine.load(QUrl("qml/main.qml"))
     engine.rootObjects()
 
