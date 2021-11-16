23f4c26a35e7a71e47a879366d622d3bb6358aa8
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-11-16 15:02:13 +0300

    Added __null method - check value on null.

--- service/RequestConstructor.py
+++ service/RequestConstructor.py
@@ -2,12 +2,15 @@ from service.Requests import Request, requests
 
 
 class RequestConstructor:
+    @staticmethod
+    def __null(var):
+        return var if var != '' else None
 
     @classmethod
     def create(cls, request_type, **kwargs):
         data = dict()
         for field in kwargs:
-            data[field] = kwargs[field]
+            data[field] = cls.__null(kwargs[field])
         return Request.construct(
             type=request_type,
             data=requests[request_type].parse_obj(data)
