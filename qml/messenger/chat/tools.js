9585362ebeab1c54fee5fd3bf39a7abcacc3df37
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-11-27 23:52:36 +0300

    Message and user time functions.

--- qml/messenger/chat/tools.js
+++ qml/messenger/chat/tools.js
@@ -1,9 +1,38 @@
 function send() {
-    if (textArea.text !== '') {
-            messagesContainer.model.append({
-            "messageText":  textArea.text,
-            "messageTime": Qt.formatTime(new Date(), "hh:mm")
-        })
-        textArea.text = ''
-    }
+    if (textArea.text !== '')
+        messenger.sendMessage(textArea.text)
+    textArea.text = ''
+}
+
+let months = {
+    0: "января",
+    10: "ноября",
+    11: "декабря",
+    9: "октября",
+    8: "сентября",
+    7: "августа",
+    6: "июля",
+    5: "июня",
+    4: "мая",
+    3: "апреля",
+    2: "марта",
+    1: "февраля"
+}
+
+function getMessageTime(timestamp) {
+    var date = new Date(timestamp)
+    if (date.getDate() == new Date().getDate())
+        return date.getHours()+":"+date.getMinutes()
+    else
+        return date.getDate() + " " + months[date.getMonth()] + " " + date.getHours()+":"+date.getMinutes()
+
+}
+
+
+function getUserTime(timestamp) {
+    let date = new Date(timestamp)
+    if (date.getDate() == new Date().getDate())
+        return "в " + date.getHours()+":"+date.getMinutes()
+    else
+        return date.getDate() + " " + months[date.getMonth()] + " в " + date.getHours()+":"+date.getMinutes()
 }
