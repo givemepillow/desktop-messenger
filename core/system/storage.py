e1b6989f60d0d9d0a2b445f5fe95b80a7a72896d
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-11-29 05:49:21 +0300

    Created storage system base on sqlite3 in-memory/file.

--- core/system/storage.py
+++ core/system/storage.py
@@ -1,33 +1,72 @@
 import sqlite3
+import os
 
 
-class Connection:
+class Database:
+    __cursor = None
+
     def __init__(self, db_name):
-        self.__connection  = sqlite3.connect(db_name)
+        self.__file_db = sqlite3.connect(db_name)
+        self.__memory_db = sqlite3.connect(':memory:')
+        self.__file_db.backup( self.__memory_db)
+        self.__cursor = self.__memory_db.cursor()
 
-    def get_connection(self):
-        return self.__connection
 
-    def close_connection(self):
-        self.__connection.close()
+    def cursor(self):
+        return self.__cursor
+
+    def connection(self):
+        return self.__memory_db
+        
+
+    def close(self):
+        self.__cursor.close()
+        self.__memory_db.backup(self.__file_db)
+        self.__memory_db.close()
+        self.__file_db.close()
+
+
+    def __del__(self):
+        self.close()
+        print("end")
 
 
 class Storage:
     __users = set()
+    __cursor = None
+    __connection = None
+    USERS_TABLE = 'users'
+    MESSAGES_TABLE = 'messages'
     
+    @classmethod
+    def initialize(cls):
+        if not os.path.exists('data'):
+            os.makedirs('data')
+        cls.__db = Database('data/storage.db')
+        cls.__cursor = cls.__db.cursor()
+        cls.__connection = cls.__db.connection()
+        # Checking for the existence of a table.
+        cls.__cursor.execute(
+            "SELECT name FROM sqlite_master " \
+            "WHERE type='table' AND (name=? OR name=?)", (cls.USERS_TABLE, cls.MESSAGES_TABLE))
+        tables = [t[0] for t in cls.__cursor.fetchall()]
+        print(tables)
+        # Creating tables.
+        if cls.USERS_TABLE not in tables:
+            cls.__cursor.execute(f"CREATE TABLE {cls.USERS_TABLE}"\
+                "(user_id BIGINT PRIMARY KEY, login VARCHAR(25), email VARCHAR(50), "\
+                    "first_name VARCHAR(30), last_name  VARCHAR(30))")
+        if cls.MESSAGES_TABLE not in tables:
+            cls.__cursor.execute(f"CREATE TABLE {cls.MESSAGES_TABLE} "\
+                "(message_id BIGINT PRIMARY KEY, from_id INTEGER NOT NULL, "\
+                    "to_id INTEGER NOT NULL, message VARCHAR(1000), date_time DECIMAIL(17,4));")
 
-    def __init__(self):
-        self.__connection = Connection('storage.db').get_connection()
-        self.__initialize()
-        self.__load_metadata()
-
+    @classmethod
+    def save_message(cls, message_id, from_id, to_id, message, date_time):
+        cls.__cursor.execute("INSERT INTO messages(message_id, from_id, to_id, message, date_time)" \
+        "VALUES(?, ?, ?, ?, ?);", (message_id, from_id, to_id, message, date_time))
+        cls.__connection.commit()
     
-    def __initialize(self):
-        ...
-
-    def __load_metadata(self):
-        ...
-
-
-    def save_message(self, from_id, message, date_time, index=None):
-        ...
+    @classmethod
+    def close(cls):
+        cls.__db.close()
