489ddcfb1c69b8b2df601aeba39e7f67aaefd5ab
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-11-29 05:50:37 +0300

    Storage initialization and save.

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterType
 from PySide6.QtCore import QUrl
 
 from core import Service, Messenger
+from core.system import Storage
 
 if __name__ == '__main__':
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
 
-    s = Service()
-    engine.rootContext().setContextProperty("service", s)
+    Storage.initialize()
+    service = Service()
+
+    engine.rootContext().setContextProperty("service", service)
     qmlRegisterType(Messenger, 'Messenger', 1, 0, 'Messenger')
+
     engine.load(QUrl("qml/main.qml"))
     engine.rootObjects()
 
     if not engine.rootObjects():
+        Storage.close()
         sys.exit(-1)
-    sys.exit(app.exec())
+    
+    status = app.exec()
+    Storage.close()
+    sys.exit(status)
